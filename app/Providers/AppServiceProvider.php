217c462d4c88c3a0fb2151eb580400bbb38426cb
Tuanldas <tuanldas@gmail.com>
2024-11-26 16:59:28 +0700

    Bind servies

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Domain\Services\MessageQueueServiceInterface;
+use App\ExternalServices\RabbitMQService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(
+            MessageQueueServiceInterface::class,
+            RabbitMQService::class
+        );
     }
 
     /**

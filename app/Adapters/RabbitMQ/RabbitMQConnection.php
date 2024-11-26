493ba505381339875e1442296034f17ba14b52a3
Tuanldas <tuanldas@gmail.com>
2024-11-26 16:58:59 +0700

    Tạo class để kết nối với rabbitmq

---
+++ app/Adapters/RabbitMQ/RabbitMQConnection.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Adapters\RabbitMQ;
+
+use PhpAmqpLib\Channel\AbstractChannel;
+use PhpAmqpLib\Channel\AMQPChannel;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+
+class RabbitMQConnection
+{
+    private AMQPStreamConnection $connection;
+    private AbstractChannel|AMQPChannel $channel;
+
+    /**
+     * @throws \Exception
+     */
+    public function __construct(string $host, int $port, string $user, string $password)
+    {
+        $this->connection = new AMQPStreamConnection($host, $port, $user, $password);
+        $this->channel = $this->connection->channel();
+    }
+
+    public function close(): void
+    {
+        $this->channel->close();
+        $this->connection->close();
+    }
+
+    public function getChannel(): AMQPChannel|AbstractChannel
+    {
+        return $this->channel;
+    }
+
+    public function getConnection(): AMQPStreamConnection
+    {
+        return $this->connection;
+    }
+}

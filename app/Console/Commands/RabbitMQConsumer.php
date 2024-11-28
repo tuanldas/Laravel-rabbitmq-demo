83ad42c701383a0047a5aebbacacc7422d7e413c
Tuanldas <tuanldas@gmail.com>
2024-11-28 09:54:05 +0700

    Ghi queue log

--- app/Console/Commands/RabbitMQConsumer.php
+++ app/Console/Commands/RabbitMQConsumer.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use App\Domain\Services\MessageQueueServiceInterface;
 use App\Models\Checkin;
+use App\Models\QueueLog;
 use Illuminate\Console\Command;
 
 class RabbitMQConsumer extends Command
@@ -24,6 +25,12 @@ class RabbitMQConsumer extends Command
         );
         try {
             $messageQueueService->consumer('mattermost.attendance.user_checkin', function ($msg) {
+                $this->info('Received message: ' . $msg->body);
+                $queueLog = new QueueLog();
+                $queueLog->action = 'consume';
+                $queueLog->queue = 'mattermost.attendance.user_checkin';
+                $queueLog->message = $msg->body;
+                $queueLog->save();
                 $data = json_decode($msg->body);
                 $checkin = new Checkin();
                 $checkin->user_id = $data->id;
---
+++ app/Models/QueueLog.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class QueueLog extends Model
+{
+    protected $table = 'queue_logs';
+}
